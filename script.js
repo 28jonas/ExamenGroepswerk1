2d2a12ea4c5e9b173731288780645e4f72caa9e9
RenautMestdagh <renaut.mestdagh@hotmail.com>
2024-12-02 16:05:20 +0100

    some reformating

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const continentSelector = document.getElementById("continentSelector")
 const countrySearch = document.getElementById("countrySearch")
+
 let countries = [];
 
 getData();
 
 
 countrySearch.addEventListener("input", () => {
+    // Verwijder alle niet-letters met een regex, maar sta ook diakritische tekens toe (zoals ë, ç, etc.)
+    this.value = this.value.replace(/[^a-zA-Z\u00C0-\u017F\s-]/g, ''); // Unicode range for accented characters
+
     const filteredCountries = applyFilters();
     setCountries(filteredCountries);
 });
